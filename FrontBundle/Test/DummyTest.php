deed7672c8e8a4130c9a724da61bc9799b4a94a0
itkg-bfouche <benjamin.fouche@businessdecision.com>
2014-08-13 15:54:38 +0200

    add test

---
+++ FrontBundle/Test/DummyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PHPOrchestra\FrontBundle\Test;
+
+/**
+ * Class DummyTest
+ */
+class DummyTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Dummy test
+     */
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
