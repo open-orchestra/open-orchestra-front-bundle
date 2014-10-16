9f57f073d55552eb95f9bc9461a27f229d4c4048
Nicolas Thal <nico.th4l@gmail.com>
2014-10-16 17:57:12 +0200

    update test for both front pages

--- FrontBundle/FunctionalTest/Controller/NodeControllerTest.php
+++ FrontBundle/FunctionalTest/Controller/NodeControllerTest.php
@@ -19,6 +19,21 @@ class NodeControllerTest extends WebTestCase
         $crawler = $client->request('GET', '');
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Bienvenu sur le site de démo issu des fixtures.")')->count());
+        $this->assertEquals(0, $crawler->filter('html:contains("Business & Decision est un Groupe international de services numériques")')->count());
+    }
+
+    /**
+     * Test fixture_home
+     */
+    public function testShowActionFixtureHomeSecondSite()
+    {
+        $client = static::createClient();
+        $client->setServerParameter('SYMFONY__SITE', '2');
+
+        $crawler = $client->request('GET', '');
+
+        $this->assertEquals(0, $crawler->filter('html:contains("Bienvenu sur le site de démo issu des fixtures.")')->count());
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Business & Decision est un Groupe international de services numériques")')->count());
     }
 
     /**
