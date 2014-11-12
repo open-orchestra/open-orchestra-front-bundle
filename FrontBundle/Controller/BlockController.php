6efbc9e0ca6c62f875dda20688b2e87b19ba401f
Nicolas Thal <nico.th4l@gmail.com>
2014-11-12 14:58:35 +0100

    add preview

--- FrontBundle/Controller/BlockController.php
+++ FrontBundle/Controller/BlockController.php
@@ -3,6 +3,7 @@
 namespace PHPOrchestra\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as Config;
@@ -15,8 +16,9 @@ class BlockController extends Controller
     /**
      * Display the response linked to a block
      *
-     * @param string $nodeId
-     * @param string $blockId
+     * @param Request $request
+     * @param string  $nodeId
+     * @param string  $blockId
      *
      * @Config\Route("/block/{nodeId}/{blockId}", name="php_orchestra_front_block")
      * @Config\Method({"GET"})
@@ -24,10 +26,15 @@ class BlockController extends Controller
      * @throws NotFoundHttpException
      * @return Response
      */
-    public function showAction($nodeId, $blockId)
+    public function showAction(Request $request, $nodeId, $blockId)
     {
-        $node = $this->get('php_orchestra_model.repository.node')
-            ->findWithPublishedAndLastVersionAndSiteId($nodeId);
+        if ($token = $request->get('token')) {
+            $decryptedToken = $this->get('php_orchestra_base.manager.encryption')->decrypt($token);
+            $node = $this->get('php_orchestra_model.repository.node')->find($decryptedToken);
+        } else {
+            $node = $this->get('php_orchestra_model.repository.node')
+                ->findWithPublishedAndLastVersionAndSiteId($nodeId);
+        }
 
         if (null !== ($block = $node->getBlocks()->get($blockId))) {
             return $this->get('php_orchestra_display.display_block_manager')
