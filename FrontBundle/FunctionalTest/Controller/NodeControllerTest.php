5a16564785b6045bd9b573edae04ba53b2b279f5
Nicolas Thal <nico.th4l@gmail.com>
2014-12-23 10:53:57 +0100

    little factorization

--- FrontBundle/FunctionalTest/Controller/NodeControllerTest.php
+++ FrontBundle/FunctionalTest/Controller/NodeControllerTest.php
@@ -9,15 +9,23 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class NodeControllerTest extends WebTestCase
 {
+    protected $client;
+
+    /**
+     * Set up test
+     */
+    public function setUp()
+    {
+        $this->client = static::createClient();
+        $this->client->setServerParameter('SYMFONY__SITE', '1');
+    }
+
     /**
      * Test fixture_home
      */
     public function testShowActionFixtureHome()
     {
-        $client = static::createClient();
-        $client->setServerParameter('SYMFONY__SITE', '1');
-
-        $crawler = $client->request('GET', '');
+        $crawler = $this->client->request('GET', '');
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Bienvenu sur le site de démo issu des fixtures.")')->count());
         $this->assertEquals(0, $crawler->filter('html:contains("Business & Decision est un Groupe international de services numériques")')->count());
@@ -28,10 +36,9 @@ class NodeControllerTest extends WebTestCase
      */
     public function testShowActionFixtureHomeSecondSite()
     {
-        $client = static::createClient();
-        $client->setServerParameter('SYMFONY__SITE', '2');
+        $this->client->setServerParameter('SYMFONY__SITE', '2');
 
-        $crawler = $client->request('GET', '');
+        $crawler = $this->client->request('GET', '');
 
         $this->assertEquals(0, $crawler->filter('html:contains("Bienvenu sur le site de démo issu des fixtures.")')->count());
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Business & Decision est un Groupe international de services numériques")')->count());
@@ -46,66 +53,34 @@ class NodeControllerTest extends WebTestCase
      */
     public function testShowActionFixtureFull()
     {
-        $client = static::createClient();
-        $client->setServerParameter('SYMFONY__SITE', '1');
-
-        $crawler = $client->request('GET', '');
+        $crawler = $this->client->request('GET', '');
         $form = $crawler->selectButton('Connexion')->form();
         $form['_username'] = 'nicolas';
         $form['_password'] = 'nicolas';
-        $crawler = $client->submit($form);
+        $crawler = $this->client->submit($form);
 
-        $crawler = $client->request('GET', '/fixture-full');
+        $crawler = $this->client->request('GET', '/fixture-full');
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Qui sommes-nous?")')->count());
-
         $this->assertGreaterThanOrEqual(11, $crawler->filter('a')->count());
 
         $link = $crawler->filter('a:contains("Fixture B&D")')->eq(1)->link();
-
-        $crawler = $client->click($link);
-
+        $crawler = $this->client->click($link);
         //$this->assertGreaterThan(0, $crawler->filter('html:contains("Tout sur B&D ")')->count());
-    }
 
-    /**
-     * Test fixture_full
-     * if there are the good text
-     * send search with fixture
-     * test the number of link
-     * if there are the good text
-     * clique on a link with text Nous contacter
-     * test if the status page is 404
-     */
-    public function testShowActionFixtureFull2()
-    {
-        $client = static::createClient();
-        $client->setServerParameter('SYMFONY__SITE', '1');
-
-        $crawler = $client->request('GET', '');
-        $form = $crawler->selectButton('Connexion')->form();
-        $form['_username'] = 'nicolas';
-        $form['_password'] = 'nicolas';
-        $crawler = $client->submit($form);
-
-        $crawler = $client->request('GET', '/node/fixture_full');
+        $crawler = $this->client->request('GET', '/fixture-full');
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Aliquam convallis facilisis nulla")')->count());
 
         $form = $crawler->selectButton('Rechercher')->form();
-
         $form['autocomplete_search[terms]'] = 'fixture';
-
-        $crawler = $client->submit($form);
+        $crawler = $this->client->submit($form);
 
         $this->assertGreaterThanOrEqual(14, $crawler->filter('a')->count());
 
 //        $this->assertGreaterThan(0, $crawler->filter('html:contains("Le bottin mondain")')->count());
-
 //        $link = $crawler->filter('a:contains("Fixture Contact Us")')->eq(1)->link();
-
 //        $client->click($link);
-
 //        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
@@ -119,10 +94,7 @@ class NodeControllerTest extends WebTestCase
      */
     public function testShowActionSearch()
     {
-        $client = static::createClient();
-        $client->setServerParameter('SYMFONY__SITE', '1');
-
-        $crawler = $client->request('GET', '/node/fixture_search');
+        $crawler = $this->client->request('GET', '/node/fixture_search');
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Aucun résultat trouvé")')->count());
 
@@ -130,7 +102,7 @@ class NodeControllerTest extends WebTestCase
 
         $form['autocomplete_search[terms]'] = 'lorem';
 
-        $crawler = $client->submit($form);
+        $crawler = $this->client->submit($form);
 
         $this->assertGreaterThanOrEqual(14, $crawler->filter('a')->count());
 
@@ -138,7 +110,7 @@ class NodeControllerTest extends WebTestCase
 
 //        $link = $crawler->filter('a:contains("Directory")')->link();
 
-//        $client->click($link);
+//        $this->client->click($link);
 
 //        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
