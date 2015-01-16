df7a93f37ed3971f80fdb6c9971c5876a363458d
itkg-ngilain <noel.gilain@businessdecision.com>
2015-01-16 18:26:20 +0100

    First xml version

--- FrontBundle/Command/OrchestraGenerateSitemapCommand.php
+++ FrontBundle/Command/OrchestraGenerateSitemapCommand.php
@@ -3,10 +3,15 @@
 namespace PHPOrchestra\FrontBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-//use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use PHPOrchestra\ModelInterface\Model\SiteInterface;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use PHPOrchestra\ModelInterface\Model\NodeInterface;
 
 class OrchestraGenerateSitemapCommand extends ContainerAwareCommand
 {
@@ -18,7 +23,7 @@ class OrchestraGenerateSitemapCommand extends ContainerAwareCommand
         $this
             ->setName('orchestra:sitemap:generate')
             ->setDescription('Generate sitemaps')
-            ->addOption('siteId', null, InputOption::VALUE_OPTIONAL, 'If set, will only generate sitemap for this site');
+            ->addOption('siteId', null, InputOption::VALUE_REQUIRED, 'If set, will only generate sitemap for this site');
     }
 
     /**
@@ -30,11 +35,17 @@ class OrchestraGenerateSitemapCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         if ($siteId = $input->getOption('siteId')) {
-            $this->generateSitemap($siteId, $output);
+
+            $site = $this->getContainer()->get('php_orchestra_model.repository.site')->findOneBySiteId($siteId);
+            $this->generateSitemap($site, $output);
+
         } else {
-            $sites = array('1', '3', '6', '9');
-            foreach ($sites as $siteId) {
-                $this->generateSitemap($siteId, $output);
+
+            $siteCollection = $this->getContainer()->get('php_orchestra_model.repository.site')->findByDeleted(false);
+            if ($siteCollection) {
+                foreach ($siteCollection as $site) {
+                    $this->generateSitemap($site, $output);
+                }
             }
         }
 
@@ -42,21 +53,57 @@ class OrchestraGenerateSitemapCommand extends ContainerAwareCommand
     }
 
     /**
-     * Generate sitemap for siteId
+     * Generate sitemap for $site
      * 
-     * @param string          $siteId
+     * @param SiteInterface   $site
      * @param OutputInterface $output
      */
-    protected function generateSitemap($siteId, OutputInterface $output)
+    protected function generateSitemap(SiteInterface $site, OutputInterface $output)
     {
-        $output->writeln("<info>Generating sitemap for siteId " . $siteId . "</info>");
+        $output->writeln("<info>Generating sitemap for site " . $site->getSiteId() . " on domain " . $site->getDomain() . "</info>");
 
-        $filename = 'sitemap.' . $siteId . '.xml';
-        $text = 'random xml content';
+        $nodes = $this->getSitemapNodesFromSite($site);
+        $filename = 'sitemap.' . $site->getDomain() . '.xml';
 
-        file_put_contents('web/' . $filename, $text);
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new GetSetMethodNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+ 
+        file_put_contents('web/' . $filename, $serializer->serialize($nodes, 'xml'));
         $output->writeln("<comment>-> " . $filename . " generated</comment>\n");
 
         return true;
+    }
+
+    /**
+     * Return an array of sitemapNodes for $site
+     * 
+     * @param SiteInterface $site
+     * 
+     * @return array
+     */
+    protected function getSitemapNodesFromSite(SiteInterface $site)
+    {
+        $nodes = array();
+
+        // TODO : récupérer les noeuds en version published uniquement + vision publique
+        $nodesCollection = $this->getContainer()->get('php_orchestra_model.repository.node')
+            ->findLastVersionBySiteId(NodeInterface::TYPE_DEFAULT, $site->getSiteId());
+
+        if ($nodesCollection) {
+            foreach($nodesCollection as $node) {
+                if ($lastmod = $node->getUpdatedAt())
+                    $lastmod = $lastmod->format('Y-m-d');
+
+                $nodes[] = array(
+                    'loc' => $site->getDomain() . '/' . $node->getPath(),
+                    'lastmod' => $lastmod,
+                    'changefreq' => $node->getSitemapChangefreq(),
+                    'priority' => $node->getSitemapPriority()
+                );
+            }
+        }
+
+        return $nodes;
     }
 }
