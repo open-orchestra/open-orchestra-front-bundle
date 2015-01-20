455f1c6823a0a963870d07f148126bee24d241db
itkg-ngilain <noel.gilain@businessdecision.com>
2015-01-20 18:28:22 +0100

    Add command

---
+++ FrontBundle/Command/OrchestraGenerateRobotsCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace PHPOrchestra\FrontBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use PHPOrchestra\ModelInterface\Model\SiteInterface;
+
+/**
+ * Class OrchestraGenerateRobotsCommand
+ *
+ */
+class OrchestraGenerateRobotsCommand extends ContainerAwareCommand
+{
+    /**
+     * Configure command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('orchestra:robots:generate')
+            ->setDescription('Generate all robots.txt')
+            ->addOption('siteId', null, InputOption::VALUE_REQUIRED, 'If set, will generate robots.txt only for this site');
+    }
+
+    /**
+     * Execute command
+     * 
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($siteId = $input->getOption('siteId')) {
+            $site = $this->getContainer()->get('php_orchestra_model.repository.site')->findOneBySiteId($siteId);
+            if ($site) {
+                $this->generateRobots($site, $output);
+            } else {
+                $output->writeln("<error>No website found with siteId " . $siteId . ".</error>");
+            }
+        } else {
+            $siteCollection = $this->getContainer()->get('php_orchestra_model.repository.site')->findByDeleted(false);
+            if ($siteCollection) {
+                foreach ($siteCollection as $site) {
+                    $this->generateRobots($site, $output);
+                }
+            }
+        }
+
+        $output->writeln("<info>Done.</info>");
+    }
+
+    /**
+     * Call robots.txt generation for $site
+     * 
+     * @param SiteInterface $site
+     * @param OutputInterface $output
+     */
+    protected function generateRobots(SiteInterface $site, OutputInterface $output)
+    {
+        $robotsManager = $this->getContainer()->get('php_orchestra_front.manager.robots');
+        $output->writeln("<info>Generating robots file for siteId " . $site->getSiteId() . " on domain " . $site->getDomain() . "</info>");
+
+        $filename = $robotsManager->generateRobots($site);
+
+        $output->writeln("<comment>-> " . $filename . " generated</comment>\n");
+    }
+}
---
+++ FrontBundle/Manager/RobotsManager.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PHPOrchestra\FrontBundle\Manager;
+
+use Symfony\Component\Filesystem\Filesystem;
+use PHPOrchestra\ModelInterface\Model\SiteInterface;
+
+/**
+ * Class RobotsManager
+ */
+class RobotsManager
+{
+    protected $filesystem;
+
+    /**
+     * @param Filesystem  $filesystem
+     */
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * Generate robots.txt for $site
+     * 
+     * @param SiteInterface   $site
+     */
+    public function generateRobots(SiteInterface $site)
+    {
+        $txtContent = $site->getRobotsTxt();
+        $filename = 'robots.' . $site->getDomain() . '.txt';
+        $this->filesystem->dumpFile('web/' . $filename, $txtContent);
+        return $filename;
+    }
+}
