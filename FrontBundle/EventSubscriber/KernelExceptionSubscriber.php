bb38f004b04d4016a794e324cbf476b31abe146e
itkg-ngilain <noel.gilain@businessdecision.com>
2015-07-27 15:36:52 +0200

    Add KernelExceptionSubscriber

---
+++ FrontBundle/EventSubscriber/KernelExceptionSubscriber.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace OpenOrchestra\FrontBundle\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use OpenOrchestra\ModelInterface\Repository\SiteRepositoryInterface;
+use OpenOrchestra\ModelInterface\Repository\ReadNodeRepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use OpenOrchestra\ModelInterface\Model\ReadSiteInterface;
+use OpenOrchestra\ModelInterface\Model\ReadNodeInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class KernelExceptionSubscriber
+ */
+class KernelExceptionSubscriber implements EventSubscriberInterface
+{
+    protected $siteRepository;
+    protected $nodeRepository;
+    protected $templating;
+    protected $request;
+
+    /**
+     * @param SiteRepositoryInterface     $siteRepository
+     * @param ReadNodeRepositoryInterface $nodeRepository
+     * @param EngineInterface             $templating
+     * @param RequestStack                $requestStack
+     */
+    public function __construct(
+        SiteRepositoryInterface $siteRepository,
+        ReadNodeRepositoryInterface $nodeRepository,
+        EngineInterface $templating,
+        RequestStack $requestStack
+    ) {
+        $this->siteRepository = $siteRepository;
+        $this->nodeRepository = $nodeRepository;
+        $this->templating = $templating;
+        $this->request = $requestStack->getMasterRequest();
+    }
+
+    /**
+     * If exception type is 404, display the Orchestra 404 node instead of Symfony exception
+     * 
+     * @param GetResponseForExceptionEvent $event
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        if ('404' == $event->getException()->getStatusCode()) {
+            $currentSite = $this->siteRepository->findByAliasDomain($this->request->getHost());
+
+            if ($currentSite) {
+                $language = $this->getLanguage($currentSite);
+                $siteId = $currentSite->getSiteId();
+                $nodeId = ReadNodeInterface::ERROR_404_NODE_ID;
+                $node = $this->nodeRepository->findOnePublishedByNodeIdAndLanguageAndSiteIdInLastVersion($nodeId, $language, $siteId);
+
+                if ($node) {
+                    $html = $this->templating->render(
+                        'OpenOrchestraFrontBundle:Node:show.html.twig',
+                        array(
+                            'node' => $node,
+                            'parameters' => array('siteId' => $node->getSiteId(), 'language' => $node->getLanguage())
+                        )
+                    );
+                    $event->setResponse(new Response($html, 404));
+                }
+            }
+        }
+    }
+
+    /**
+     * Get the language to display the 404 page for $site
+     * If a language prefix maps the url, get the corresponding language
+     * Else get the language of the main alias
+     * 
+     * @param ReadSiteInterface $site
+     */
+    protected function getLanguage(ReadSiteInterface $site)
+    {
+        $targetAlias = null;
+        $path = trim($this->request->getPathInfo(), '/');
+        $host = $this->request->getHost();
+
+        if ('' != $path) {
+            foreach ($site->getAliases() as $alias) {
+                if ($host == $alias->getDomain() && strpos($path . '/', $alias->getPrefix() . '/') === 0) {
+                    $targetAlias = $alias;
+                    break;
+                }
+            }
+        }
+
+        if (!$targetAlias) {
+            $targetAlias = $site->getMainAlias();
+        }
+
+        return $targetAlias->getLanguage();
+    }
+
+    /**
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::EXCEPTION => 'onKernelException'
+        );
+    }
+}
---
+++ FrontBundle/Tests/EventSubscriber/KernelExceptionSubscriberTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace OpenOrchestra\FrontBundle\Tests\EventSubscriber;
+
+use OpenOrchestra\FrontBundle\EventSubscriber\KernelExceptionSubscriber;
+use Phake;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Test KernelExceptionSubscriber
+ */
+class KernelExceptionSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var KernelExceptionSubscriber
+     */
+    protected $subscriber;
+
+    protected $siteRepository;
+    protected $site;
+    protected $mainAlias;
+    protected $nodeRepository;
+    protected $templating;
+    protected $requestStack;
+    protected $request;
+    protected $event;
+    protected $exception;
+
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        $this->mainAlias = Phake::mock('OpenOrchestra\ModelInterface\Model\SiteAliasInterface');
+        Phake::when($this->mainAlias)->getLanguage()->thenReturn('en');
+        $this->site = Phake::mock('OpenOrchestra\ModelInterface\Model\ReadSiteInterface');
+        Phake::when($this->site)->getMainAlias()->thenReturn($this->mainAlias);
+        $this->siteRepository = Phake::mock('OpenOrchestra\ModelInterface\Repository\SiteRepositoryInterface');
+        Phake::when($this->siteRepository)->findByAliasDomain(Phake::anyParameters())->thenReturn($this->site);
+
+        $this->nodeRepository = Phake::mock('OpenOrchestra\ModelInterface\Repository\ReadNodeRepositoryInterface');
+        $this->templating = Phake::mock('Symfony\Bundle\FrameworkBundle\Templating\EngineInterface');
+
+        $this->request = Phake::mock('Symfony\Component\HttpFoundation\Request');
+        $this->requestStack = Phake::mock('Symfony\Component\HttpFoundation\RequestStack');
+        Phake::when($this->requestStack)->getMasterRequest()->thenReturn($this->request);
+
+        $this->exception = Phake::mock('Symfony\Component\HttpKernel\Exception\HttpException');
+        $this->event = Phake::mock('Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent');
+        Phake::when($this->event)->getException()->thenReturn($this->exception);
+
+        $this->subscriber = new KernelExceptionSubscriber($this->siteRepository, $this->nodeRepository, $this->templating, $this->requestStack);
+    }
+
+    /**
+     * Test instance
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf('Symfony\Component\EventDispatcher\EventSubscriberInterface', $this->subscriber);
+    }
+
+    /**
+     * Test subscribed events
+     */
+    public function testSubscribedEvent()
+    {
+        $this->assertArrayHasKey(KernelEvents::EXCEPTION, $this->subscriber->getSubscribedEvents());
+        $this->assertTrue(method_exists($this->subscriber, 'onKernelException'));
+    }
+
+    /**
+     * @param string                                               $status
+     * @param OpenOrchestra\ModelInterface\Model\ReadNodeInterface $node
+     * @param int                                                  $expectedResponseCount
+     * 
+     * @dataProvider getErrorContext
+     */
+    public function testOnKernelException($status, $node, $expectedResponseCount)
+    {
+        Phake::when($this->exception)->getStatusCode()->thenReturn($status);
+        Phake::when($this->nodeRepository)->findOnePublishedByNodeIdAndLanguageAndSiteIdInLastVersion(Phake::anyParameters())->thenReturn($node);
+
+        $this->subscriber->onKernelException($this->event);
+
+        Phake::verify($this->event, Phake::times($expectedResponseCount))->setResponse(Phake::anyParameters());
+    }
+
+    /**
+     * Provide error context
+     */
+    public function getErrorContext()
+    {
+        $node = Phake::mock('OpenOrchestra\ModelInterface\Model\ReadNodeInterface');
+
+        return array(
+            array('404', null, 0),
+            array('404', $node, 1),
+            array('500', null, 0),
+            array('500', $node, 0),
+        );
+    }
+}
