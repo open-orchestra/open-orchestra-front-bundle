ce42f0fc1fd6020b7f814dc6d600212580a5872b
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-03-30 14:07:53 +0200

    fix test

--- FrontBundle/Tests/Functional/Command/OrchestraGenerateErrorPagesCommandTest.php
+++ FrontBundle/Tests/Functional/Command/OrchestraGenerateErrorPagesCommandTest.php
@@ -10,43 +10,49 @@ use OpenOrchestra\BaseBundle\Tests\AbstractTest\AbstractKernelTestCase;
  */
 class OrchestraGenerateErrorPagesCommandTest extends AbstractKernelTestCase
 {
+    protected $command;
+
+    /**
+     * Set Up
+     */
+    public function setUp()
+    {
+        static::bootKernel();
+        $application = new Application(static::$kernel);
+        $application->add(new OrchestraGenerateErrorPagesCommand());
+        $this->command = $application->find('orchestra:errorpages:generate');
+    }
+
     /**
      * Test the command
      *
-     * @param array $siteAlias an array formated as $siteId => $aliasCount
+     * @param string $siteId
      *
      * @dataProvider provideSiteAlias
      */
-    public function testExecute(array $siteAlias)
+    public function testExecute($siteId)
     {
-        $kernel = static::createKernel(array('environment' => 'test' ,'debug' => false));
-        $kernel->boot();
+        $commandTester = new CommandTester($this->command);
+        $commandTester->execute(array('command' => $this->command->getName()));
+        $this->assertRegExp(
+            '/Generating error pages for siteId ' . $siteId . '/',
+            $commandTester->getDisplay()
+        );
 
-        $application = new Application($kernel);
-        $application->add(new OrchestraGenerateErrorPagesCommand());
+        $site = static::$kernel->getContainer()->get('open_orchestra_model.repository.site')->findOneBySiteId($siteId);
+        $aliases = $site->getAliases();
 
-        $command = $application->find('orchestra:errorpages:generate');
-        $commandTester = new CommandTester($command);
-        $commandTester->execute(array('command' => $command->getName()));
-
-        foreach ($siteAlias as $siteId => $aliasCount) {
+        foreach ($aliases as $key => $alias) {
             $this->assertRegExp(
-                '/Generating error pages for siteId ' . $siteId . '/',
+                '/-> ' . $siteId . '\/alias-' . $key . '\/Error 404.html generated/',
                 $commandTester->getDisplay()
             );
-
-            for ($i = 0; $i < $aliasCount; $i++) {
-                $this->assertRegExp(
-                    '/-> ' . $siteId . '\/alias-' . $i . '\/Error 404.html generated/',
-                    $commandTester->getDisplay()
-                );
-                $this->assertFileExists('./web/' . $siteId . '/alias-' . $i . '/Error 404.html');
-                $this->assertRegExp(
-                    '/-> ' . $siteId . '\/alias-' . $i . '\/Error 503.html generated/',
-                    $commandTester->getDisplay()
-                );
-                $this->assertFileExists('./web/' . $siteId . '/alias-' . $i . '/Error 503.html');
-            }
+            $this->assertFileExists('./web/' . $siteId . '/alias-' . $key . '/Error 404.html');
+            $this->assertRegExp(
+                '/-> ' . $siteId . '\/alias-' . $key . '\/Error 503.html generated/',
+                $commandTester->getDisplay()
+            );
+            $this->assertFileExists('./web/' . $siteId . '/alias-' . $key . '/Error 503.html');
         }
 
         $this->assertRegExp('/Done./', $commandTester->getDisplay());
@@ -58,7 +64,7 @@ class OrchestraGenerateErrorPagesCommandTest extends AbstractKernelTestCase
     public function provideSiteAlias()
     {
         return array(
-            array(array(2 => 9)),
+            array('2'),
         );
     }
 }
